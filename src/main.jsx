2c962fca7d96492519bb9f21e4aa6862816058b6
Yeasir-Hossain <yeasir06@gmail.com>
2023-08-23 23:27:39 +0600

    cart and checkout with error pushed

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { router } from "./routes/routes";
 import { Toaster } from "react-hot-toast";
 import { UserProvider } from "./contexts/user/UserContext";
+import { CartProvider } from "./contexts/cart/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <>
         <UserProvider>
-            <RouterProvider router={router} />
+            <CartProvider>
+                <RouterProvider router={router} />
+            </CartProvider>
         </UserProvider>
         <Toaster
             position="top-right"
