48e10e176c191ee614ca577dd3943ea9cb76dad7
yeasir-coredevs <yeasir_hossain@coredevs.ltd>
2023-08-25 17:07:07 +0600

    done

--- src/Dashboard/Pages/NewProduct.jsx
+++ src/Dashboard/Pages/NewProduct.jsx
@@ -24,8 +24,8 @@ import { useTitle } from "../../Components/Hooks/useTitle";
 const NewProduct = () => {
   useTitle("New Product");
   const { productId } = useParams();
-  const [product,setProduct]= useState(null);
-  const [btnType,setBtnType]= useState('submit')
+  const [product, setProduct] = useState(null);
+  const [btnType, setBtnType] = useState('submit')
   const [categories, setCategories] = useState([]);
   const [selectedCategeory, setSelectedcategory] = useState(null);
   const [selectedSubcategeory, setSelectedsubcategory] = useState(null);
@@ -35,7 +35,7 @@ const NewProduct = () => {
   });
 
 
-  
+
 
   const productForm = useFormik({
     initialValues: {
@@ -58,48 +58,48 @@ const NewProduct = () => {
       ) {
         return;
       }
-      values.status=btnType;
+      values.status = btnType;
       values.category = selectedCategeory;
       values.subcategory = selectedSubcategeory;
       removeEmptyFields(values);
-      const { images, ...rest } = values; 
-      product?  terminal.request({ name: 'updateProduct', params: { id: product?.id}, body: { data: rest }}).then(res=>res?.status===false?toaster({ type: 'success', message: res.message}):toaster({ type: 'success', message: 'Product successfully Updated'})) : terminal.request({ name: 'registerProduct', body: { data: rest, images}}).then(res=>res?.status===false?toaster({ type: 'success', message: res.message}):toaster({ type: 'success', message: 'Product successfully added'}))
+      const { images, ...rest } = values;
+      product ? terminal.request({ name: 'updateProduct', params: { id: product?.id }, body: { data: rest } }).then(res => res?.status === false ? toaster({ type: 'success', message: res.message }) : toaster({ type: 'success', message: 'Product successfully Updated' })) : terminal.request({ name: 'registerProduct', body: { data: rest, images } }).then(res => res?.status === false ? toaster({ type: 'success', message: res.message }) : toaster({ type: 'success', message: 'Product successfully added' }))
 
     },
   });
 
   useEffect(() => {
-    terminal.request({ name: 'allCategory'}).then(res=> setCategories(res));
-    if(productId){
+    terminal.request({ name: 'allCategory' }).then(res => setCategories(res));
+    if (productId) {
       terminal
         .request({ name: "singleProduct", params: { id: productId } })
-        .then((res) =>{
-          if(res.status===false) {
-            toaster({type: 'error', message: res.message});
+        .then((res) => {
+          if (res.status === false) {
+            toaster({ type: 'error', message: res.message });
           }
           else {
             setProduct(res);
             setSelectedcategory(res.category.id);
             setSelectedsubcategory(res.subcategory)
 
-          productForm.setValues({
-            "name": res.name,
-            "description": res.description,
-            "price": res.price,
-            "tax": res.tax,
-            "fee": res.fee,
-            "quantity": res.quantity,
-            "origin": res.origin,
-            "link": res.link,
-            "tags": res.tags,
-          })
+            productForm.setValues({
+              "name": res.name,
+              "description": res.description,
+              "price": res.price,
+              "tax": res.tax,
+              "fee": res.fee,
+              "quantity": res.quantity,
+              "origin": res.origin,
+              "link": res.link,
+              "tags": res.tags,
+            })
           }
-          
+
         }
         );
     }
-   
-    
+
+
   }, []);
   const handleCheck = () => {
     if (selectedCategeory === null) {
@@ -159,37 +159,39 @@ const NewProduct = () => {
 
             <h2 className="text-base text-secondary font-semibold">Category</h2>
             <div className="border border-[#0000001c] rounded-lg p-3 grid gap-3">
-            <label className="text-[#475569] text-sm">Parent Category</label>
+              <label className="text-[#475569] text-sm">Parent Category</label>
               <select
-                className={`bg-transparent border-[1px] w-full outline-none px-3 py-2 rounded-lg ${
-                  categoryError.category ? "border-[red]" : ""
-                } `}
-                onChange={(e) => {setSelectedcategory(e.target.value) , setSelectedsubcategory(null), setCategoryerror({
-                  category: false,
-                  subCategory: true,
-                });}}
+                className={`bg-transparent border-[1px] w-full outline-none px-3 py-2 rounded-lg appearance-none ${categoryError.category ? "border-[red]" : ""
+                  } `}
+                onChange={(e) => {
+                  setSelectedcategory(e.target.value), setSelectedsubcategory(null), setCategoryerror({
+                    category: false,
+                    subCategory: true,
+                  });
+                }}
                 value={selectedCategeory || ''}>
-               <option disabled value="">Select</option>
-                {categories.map((cat, i) => <option key={i} value={cat.id}>{cat.name}</option>)}
+                <option disabled value="">Select</option>
+                {categories.map((cat, i) => <option key={i} value={cat.id} >{cat.name}</option>)}
               </select>
 
               <label className="text-[#475569] text-sm">Sub Category</label>
-               <select
-                className={`bg-transparent border-[1px] w-full outline-none px-3 py-2 rounded-lg ${
-                  categoryError.subCategory ? "border-[red]" : ""
-                } `}
-                onChange={(e) => {setSelectedsubcategory(e.target.value), setCategoryerror({
-                  category: false,
-                  subCategory: false,
-                })}}
-                value={selectedSubcategeory ||   ''}>
+              <select
+                className={`bg-transparent border-[1px] w-full outline-none px-3 py-2 rounded-lg ${categoryError.subCategory ? "border-[red]" : ""
+                  } `}
+                onChange={(e) => {
+                  setSelectedsubcategory(e.target.value), setCategoryerror({
+                    category: false,
+                    subCategory: false,
+                  })
+                }}
+                value={selectedSubcategeory || ''}>
                 <option disabled value="">
                   Select
                 </option>
                 {
-                  categories?.find(item=>item.id===selectedCategeory)?.subcategory?.map(sub=> <option key={sub.id} value={sub.id}>{sub.name}</option>)
+                  categories?.find(item => item.id === selectedCategeory)?.subcategory?.map(sub => <option key={sub.id} value={sub.id}>{sub.name}</option>)
                 }
-                
+
               </select>
 
               <Input
@@ -263,7 +265,7 @@ const NewProduct = () => {
                 }
                 placeholder="0.00"
               />
-              
+
             </div>
             <h2 className="text-base text-secondary font-semibold">
               Delivery Information
@@ -287,21 +289,21 @@ const NewProduct = () => {
                 />
               </div>
               <div className="col-span-2">
-              <Input
-                    styles="basic"
-                    label="Stock"
-                    type="number"
-                    name="quantity"
-                    change={productForm.handleChange}
-                    blur={productForm.handleBlur}
-                    value={productForm.values.quantity}
-                    error={
-                      productForm.touched.from && productForm.errors.quantity
-                        ? productForm.errors.quantity
-                        : null
-                    }
-                    placeholder="0"
-                  />
+                <Input
+                  styles="basic"
+                  label="Stock"
+                  type="number"
+                  name="quantity"
+                  change={productForm.handleChange}
+                  blur={productForm.handleBlur}
+                  value={productForm.values.quantity}
+                  error={
+                    productForm.touched.from && productForm.errors.quantity
+                      ? productForm.errors.quantity
+                      : null
+                  }
+                  placeholder="0"
+                />
               </div>
             </div>
             <h2 className="text-base text-secondary font-semibold">
@@ -334,9 +336,9 @@ const NewProduct = () => {
                 Discard
               </Button>
               <div className="space-x-2">
-                <Button onClick={()=>setBtnType('archived')} style="outline" type="submit">Archive</Button>
-                <Button onClick={()=>setBtnType('draft')} style="outline" type="submit">Draft</Button>
-                <Button onClick={()=> {handleCheck(), setBtnType('active') }} style="primary" type="submit">
+                <Button onClick={() => setBtnType('archived')} style="outline" type="submit">Archive</Button>
+                <Button onClick={() => setBtnType('draft')} style="outline" type="submit">Draft</Button>
+                <Button onClick={() => { handleCheck(), setBtnType('active') }} style="primary" type="submit">
                   Publish
                 </Button>
               </div>
