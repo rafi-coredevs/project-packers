d788117f5158b56ca4ada3a41f9ddef3151b928c
rafi-coredevs <rafi_uddin@coredevs.ltd>
2023-08-25 16:10:43 +0600

    category mobile screen updated

--- src/Components/UiElements/Category/Category.jsx
+++ src/Components/UiElements/Category/Category.jsx
@@ -4,6 +4,7 @@ import arrow from '../../../assets/icons/cd-arrow-right-2.svg';
 
 const Category = ({ data, refatch }) => {
   const [expandedCategory, setExpandedCategory] = useState(null);
+  const [categoryHandler, setCategoryHandler] = useState(false)
   const toggleSubCategories = (categoryId) => {
     setExpandedCategory((prev) => (prev === categoryId ? null : categoryId));
   };
@@ -45,15 +46,42 @@ const Category = ({ data, refatch }) => {
     ))}
   </div>;
 
-  const smCategory = <div className='w-full flex items-center border rounded-full px-5 py-3'>
+  const smCategory = <>
+  <button type="button" onClick={()=> setCategoryHandler(!categoryHandler)} className='w-full flex items-center border rounded-full px-5 py-3'>
     <img className='w-5 mr-2' src={icon} alt='icon' />
     <div className='w-full flex justify-between'>
       <h4>Sort and Filter</h4>
       <img src={arrow} className='rotate-90' />
     </div>
-  </div>;
+  </button>
+  <div className={`${categoryHandler ? 'block' : 'hidden'} p-3`}>
+  {data?.map((category, i) => (
+      <div key={i} className="category">
 
+        <div
+          className={`text-[#475569] text-base font-normal cursor-pointer flex justify-between category-name ${category?.subcategory?.length > 0 ? "collapsible" : ""
+            }`}
+          onClick={() => toggleSubCategories(category.id)}
+        >
+          <p className="">
 
+            {category.name}
+          </p>
+          {category?.subcategory?.length > 0 && <img src={arrow} className='rotate-90' />}
+        </div>
+        {expandedCategory === category.id && (
+          <div className="ml-8 my-2">
+            <ul className=" flex gap-2 flex-col list-disc">
+              {category.subcategory.map((subcategory) => (
+                <li onClick={() => handleCategory({ category: category.id, subcategory: subcategory.id })} className="text-[#475569] text-base font-normal cursor-pointer" key={subcategory.slug}>{subcategory.name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    ))}
+  </div>
+  </>
 
   return <>
     <div className='hidden lg:block'>{lgCategory}</div>
