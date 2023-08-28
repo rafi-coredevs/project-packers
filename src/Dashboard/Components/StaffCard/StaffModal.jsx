53cb9df6e7fa3b8a90b25c1ad8357c33e5974935
yeasir-coredevs <yeasir_hossain@coredevs.ltd>
2023-08-28 18:48:01 +0600

    staff changes updated

--- src/Dashboard/Components/StaffCard/StaffModal.jsx
+++ src/Dashboard/Components/StaffCard/StaffModal.jsx
@@ -2,13 +2,23 @@ import React, { useState } from 'react';
 import Input from '../UiElements/Input/Input';
 import Button from '../UiElements/Button/Button';
 import UserIcon from '../../../Components/UiElements/UserIcon/UserIcon';
+import { terminal } from '../../../contexts/terminal/Terminal';
+import toaster from '../../../Util/toaster';
 
 const StaffModal = ({ setModal, user }) => {
     const totalaccess = ['support', 'product', 'order', 'request']
     const [access, setAccess] = useState(user?.access)
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(e.target);
+    const [role, setRole] = useState()
+    const handleSubmit = () => {
+        terminal.request({ name: 'updateUser', params: { id: user.id }, body: { data: { role, access } } }).then(data => {
+            if (data.id) {
+                setModal(false)
+                toaster({ type: 'success', message: 'Staff updated' })
+            }
+            else {
+                toaster({ type: 'error', message: 'An error has occurred' })
+            }
+        })
     }
     return (
         <>
@@ -27,7 +37,7 @@ const StaffModal = ({ setModal, user }) => {
                 </div>
                 <Input
                     styles="select"
-                    change={(e) => console.log(e.target.value)}
+                    change={(e) => setRole(e.target.value)}
                     option={[
                         { name: "Admin", value: "admin" },
                         { name: "Super Admin", value: "super-admin" },
@@ -36,22 +46,20 @@ const StaffModal = ({ setModal, user }) => {
                     ]}
                 />
             </div>
-            <form onSubmit={handleSubmit} className="p-5 grid gap-4 items-start">
+            <div className="p-5 grid gap-4 items-start">
                 <div className="space-x-2">
                     <input
-                        defaultChecked={access.includes('support')}
+                        checked={access.includes('support')}
                         onChange={(e) => {
                             if (e.target.checked) {
                                 setAccess(prev => [...prev, 'support'])
                             }
                             else {
-                                setAccess(prev => prev.map(prev => prev !== 'support'))
+                                setAccess(prev => prev.filter(prev => prev !== 'support'))
                             }
                         }}
                         className="accent-yellow-500"
                         type="checkbox"
-                        name=""
-                        id=""
                     />
                     <label htmlFor="" className="text-[#4F4F4F] font-normal">
                         Support ticket / life chat
@@ -59,11 +67,17 @@ const StaffModal = ({ setModal, user }) => {
                 </div>
                 <div className="space-x-2">
                     <input
-                        defaultChecked={access.includes('product')}
+                        checked={access.includes('product')}
+                        onChange={(e) => {
+                            if (e.target.checked) {
+                                setAccess(prev => [...prev, 'product'])
+                            }
+                            else {
+                                setAccess(prev => prev.filter(prev => prev !== 'product'))
+                            }
+                        }}
                         className="accent-yellow-500"
                         type="checkbox"
-                        name="product"
-                        id=""
                     />
                     <label htmlFor="" className="text-[#4F4F4F] font-normal">
                         Product upload and edit
@@ -71,11 +85,17 @@ const StaffModal = ({ setModal, user }) => {
                 </div>
                 <div className="space-x-2">
                     <input
-                        defaultChecked={access.includes('order')}
+                        checked={access.includes('order')}
+                        onChange={(e) => {
+                            if (e.target.checked) {
+                                setAccess(prev => [...prev, 'order'])
+                            }
+                            else {
+                                setAccess(prev => prev.filter(prev => prev !== 'order'))
+                            }
+                        }}
                         className="accent-yellow-500"
                         type="checkbox"
-                        name=""
-                        id=""
                     />
                     <label htmlFor="" className="text-[#4F4F4F] font-normal">
                         Order management
@@ -83,11 +103,17 @@ const StaffModal = ({ setModal, user }) => {
                 </div>
                 <div className="space-x-2">
                     <input
-                        defaultChecked={access.includes('request')}
+                        checked={access.includes('request')}
+                        onChange={(e) => {
+                            if (e.target.checked) {
+                                setAccess(prev => [...prev, 'request'])
+                            }
+                            else {
+                                setAccess(prev => prev.filter(prev => prev !== 'request'))
+                            }
+                        }}
                         className="accent-yellow-500"
                         type="checkbox"
-                        name=""
-                        id=""
                     />
                     <label htmlFor="" className="text-[#4F4F4F] font-normal">
                         Request management
@@ -95,10 +121,10 @@ const StaffModal = ({ setModal, user }) => {
                 </div>
                 <div className="text-end">
                     <Button type='submit'
-                        // onClick={() => { setModal(false) }}
+                        onClick={() => { handleSubmit() }}
                         style="green">Save & Exit</Button>
                 </div>
-            </form>
+            </div>
         </>
     );
 };
