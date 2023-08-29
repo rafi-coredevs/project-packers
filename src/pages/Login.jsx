8c433c1d9e6458f107316893171cdd2e2bdc6395
masum_khan@coredevs.ltd <masum_khan@coredevs.ltd>
2023-08-29 16:46:56 +0600

    changes in Login page. admin should go to admin page after login, normal user to the home

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
 
 	const destinedTo = location.state?.afterLogin;
 
-	const { Login, setUser } = useUserCtx();
+	const { Login, setUser, user } = useUserCtx();
 	const loginForm = useFormik({
 		initialValues: {
 			email: '',
@@ -45,11 +45,10 @@ const Login = () => {
 								body: requestItemData,
 							})
 							.then((d) => {
-								// console.log('item request response from login', d);
 								navigate('/', { state: true });
 							});
 					} else {
-						destinedTo ? navigate(destinedTo) : navigate('/');
+						destinedTo ? navigate(destinedTo) : user.role == "user" ? navigate('/') : navigate('/admin');
 					}
 				}
 			});
@@ -60,7 +59,7 @@ const Login = () => {
 			<div className='container mx-auto grid grid-cols-1 sm:grid-cols-2 relative z-20 justify-center divide-x divide-[#ffffff1a] flex-wrap'>
 				<div className='w-full sm:max-w-[30vw]'>
 					<p className='text-white text-[52px] font-sora font-extrabold'>
-						Welcome Back {JSON.stringify(destinedTo)}
+						Welcome Back
 					</p>
 					<form
 						className='flex flex-col gap-5'
