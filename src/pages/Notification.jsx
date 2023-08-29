5e6ad2971134f8bd2c0a4e27052c3c4f7c4f5cc0
yeasir-coredevs <yeasir_hossain@coredevs.ltd>
2023-08-29 18:43:44 +0600

    notification updated

--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -1,7 +1,11 @@
+import { useEffect, useState } from "react";
 import { useTitle } from "../Components/Hooks/useTitle";
 import Breadcrumb from "../Components/UiElements/Breadcrumb/Breadcrumb";
-import Icon from "../Components/UiElements/Icon/Icon";
-import cart from "../assets/icons/cd-order.svg";
+import acc from "../assets/icons/Avatar.svg";
+import prod from '../assets/icons/cd-products.svg'
+import { useUserCtx } from "../contexts/user/UserContext";
+import { terminal } from "../contexts/terminal/Terminal";
+import formatTimeAgo from "../Util/formatTimeAgo";
 const DUMMY_NOTIFICATION = [
   {
     id: 1,
@@ -34,9 +38,19 @@ const DUMMY_NOTIFICATION = [
  */
 const Notification = () => {
   useTitle("My Notifications");
-  const clickHandler = (v) => {
-    console.log(v);
-  };
+  const [notifications, setNotifications] = useState();
+  const { user } = useUserCtx();
+  const currentDate = new Date();
+  const sevenDaysAgoTimestamp = currentDate.getTime() - 7 * 24 * 60 * 60 * 1000;
+  const sevenDaysAgoDate = new Date(sevenDaysAgoTimestamp);
+  const sevenDaysAgoISO = sevenDaysAgoDate.toISOString();
+
+  useEffect(() => {
+    user?.id &&
+      terminal
+        .request({ name: 'getNotification', queries: { limit: 100, time: JSON.stringify({ '$gte': sevenDaysAgoISO }) } })
+        .then((data) => data.docs && setNotifications(data.docs));
+  }, [user]);
   return (
     <main>
       <Breadcrumb />
@@ -45,26 +59,22 @@ const Notification = () => {
           <h3 className="text-2xl font-semibold text-secondary mt-[20px] mb-[12px]">
             Notification
           </h3>
-          {DUMMY_NOTIFICATION?.map((item) => {
+          {notifications?.length > 0 && notifications?.map((item) => {
             return (
               <div
-                onClick={() => clickHandler(item.id)}
                 key={item.id}
                 className="cursor-pointer flex gap-3 py-2 border-t border-[#0000001A]"
               >
                 <div className="h-12 w-12">
-                  <Icon icon={cart} />
+                  {item.type === 'account' ? <img src={acc} /> : <img className="bg-[#CFF6EF] p-1 rounded-md" src={prod} />}
                 </div>
 
                 <div className="">
                   <p className="font-sans font-medium text-sm overflow-hidden">
-                    {item.title}
-                  </p>
-                  <p className="font-sans font-medium text-sm overflow-hidden">
-                    {item.description}
+                    {item.message}
                   </p>
                   <p className="font-sans font-normal text-xs text-[#00000066] overflow-hidden">
-                    {item.time}
+                    {formatTimeAgo(item.time)}
                   </p>
                 </div>
               </div>
