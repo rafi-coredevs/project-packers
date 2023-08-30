2000ffa50c12da54d000c6b5a196e18bdf7a3223
aHasnatDEV <abul_hasnat@coredevs.ltd>
2023-08-30 16:23:01 +0600

    ComingSoon and ReloadPage responsive fix

--- src/pages/ReloadPage.jsx
+++ src/pages/ReloadPage.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const ReloadPage = () => {
   return <>
     <div className='min-h-screen flex flex-col justify-center items-center'>
-      <div className='w-2/5 p-1 border-2 border-primary rounded-full'>
+      <div className='w-[80%] md:w-[20%] p-1 border-2 border-primary rounded-full'>
         <div className='relative rounded-full overflow-hidden'>
-          <p className='text-center font-extrabold text-5xl py-3 text-secondary'>Loading...</p>
+          <p className='text-center font-extrabold text-2xl py-3 text-secondary'>Loading...</p>
           <span className='w-[200%] h-full bg-primary absolute top-0 z-10 animate-coming' />
-          <p className='w-full text-center font-extrabold text-5xl py-3 text-outline absolute z-30 top-0'>Loading...</p>
+          <p className='w-full text-center font-extrabold text-2xl py-3 text-outline absolute z-30 top-0'>Loading...</p>
         </div>
       </div>
     </div>
