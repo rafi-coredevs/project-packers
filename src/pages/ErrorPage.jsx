3add5d7d25c4af85b35f1f03ee537c790e6d207f
aHasnatDEV <abul_hasnat@coredevs.ltd>
2023-08-30 18:43:39 +0600

    ComingSoon page update

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -8,7 +8,7 @@ const ErrorPage = () => {
         <h1 className='text-center text-7xl lg:text-[10rem] font-extrabold text-secondary flex mx-auto'>
           Oops<p className='origin-bottom animate-wiggle'>!</p>
         </h1>
-        <p className='font-semibold text-xl lg:text-2xl mt-4 mb-7'>404 - Page not Found</p>
+        <p className='font-semibold text-xl lg:text-2xl text-secondary my-10'>404 - Page not Found</p>
         <Link to='/' className='bg-primary rounded-full p-4'>Go to Homepage</Link>
       </div>
     </div>
