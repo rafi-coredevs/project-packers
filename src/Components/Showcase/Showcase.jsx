01f8ffe1176111b1a0be3aa5e01b2c328f43e0fc
aHasnatDEV <abul_hasnat@coredevs.ltd>
2023-08-30 11:47:16 +0600

    Carousel, Brands and Showcase fix

--- src/Components/Showcase/Showcase.jsx
+++ src/Components/Showcase/Showcase.jsx
@@ -6,9 +6,9 @@ const Showcase = ({ type, title, description, data }) => {
   const navigate = useNavigate();
   const item = type === "slide" ? 4 : 8;
   return (
-    <div className="container mx-auto flex flex-col items-center my-[34px] sm:my-[74px] px-4">
+    <div className="container mx-auto flex flex-col items-center mb-20 px-4">
       <Heading title={title} description={description} />
-      <div className="grid grid-cols-2 sm:grid-cols-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {data?.slice(0, item).map((item,i) => {
           return (
             <ProductCard
