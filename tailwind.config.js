1549c85ea0f6d455a362c2171b75b4c9ac89dad2
aHasnatDEV <abul_hasnat@coredevs.ltd>
2023-08-30 15:44:44 +0600

    ComingSoon page update

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@
 //       colors:{
 //         primary : '#F2C852',
 //         secondary: '#0D3D4B'
-        
+
 //       },
 //       content:{
 //         'arrowLeft': 'url(./src/assets/icons/cd-arrow-right-2.svg)'
@@ -39,7 +39,7 @@ export default {
       },
       content: {
         'arrowLeft': 'url(./src/assets/icons/cd-arrow-right-2.svg)',
-        
+
       },
 
       animation: {
@@ -48,7 +48,8 @@ export default {
         bounce: 'bounce 1s ease-in-out infinite',
         send: 'send .5s linear infinite',
         scale: 'scaleUp 1s ease-in-out infinite',
-        loading: 'loading 1s ease-in-out infinite'
+        loading: 'loading 1s ease-in-out infinite',
+        coming: 'coming 8s ease-in-out infinite'
       },
 
       keyframes: {
@@ -85,6 +86,17 @@ export default {
           },
           '50%': {
             backgroundPosition: '100% 50%'
+          }
+        },
+        coming: {
+          '0%': {
+            left: '-200%'
+          },
+          '50%':{
+            left: '100%'
+          },
+          '100%': {
+            left: '-200%'
           }
         }
       }
