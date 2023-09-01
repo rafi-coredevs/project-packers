5735ee68bb8fe84cd4a245e683e5880d99601b22
rafi-coredevs <rafi_uddin@coredevs.ltd>
2023-09-01 15:20:33 +0600

    image preview added

--- src/Components/UiElements/GalleryCard/GalleryCard.jsx
+++ src/Components/UiElements/GalleryCard/GalleryCard.jsx
@@ -1,17 +1,28 @@
 import { useEffect, useState } from "react";
 import { BASE_URL } from "../../../Util/apiCall";
-
+import errorImg from '../../../assets/noImages.svg'
 const GalleryCard = ({ data }) => {
   const [presentImage, setPresentImage] = useState(data[0]);
   useEffect(() => {
     setPresentImage(data[0]);
   }, [data]);
+
+  const handleLoading = (event) =>{
+    // console.log(`Picture successfully ${event.currentTarget.src} loaded.`);
+
+  }
+  const handleError = (event) => {
+    event.currentTarget.src = errorImg;
+    event.currentTarget.className = "flex items-center justify-center  mt-8 mx-auto"
+  }
+
   const imgFn = (imgSrc) => {
     return (
       <img
         onClick={() => setPresentImage(imgSrc)}
         className="border rounded-xl cursor-pointer duration-200 active:scale-95 w-full"
         src={`${BASE_URL}/api/${imgSrc}`}
+        onLoad={handleLoading} onError={handleError}
         alt="product image"
       />
     );
@@ -27,6 +38,7 @@ const GalleryCard = ({ data }) => {
         <img
           className="w-full  h-full border rounded-xl"
           src={`${BASE_URL}/api/${presentImage}`}
+          onLoad={handleLoading} onError={handleError}
           alt="product image"
         />
       </aside>
