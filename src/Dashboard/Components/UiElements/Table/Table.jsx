9a90b91092756843ebee5ae3d0e48de3cae94fff
Shazzad-Hossen-Coredevs <shazzad_hossen@coredevs.ltd>
2023-09-01 11:11:11 +0600

    paginate fixed

--- src/Dashboard/Components/UiElements/Table/Table.jsx
+++ src/Dashboard/Components/UiElements/Table/Table.jsx
@@ -430,7 +430,7 @@ useEffect(()=>{
           </div>
         )
       }
-      {(location !== 'category' && loading === false) && (
+      {(location !== 'category' && loading === false && data?.docs?.length!==0) && (
         <div className='flex justify-between items-center py-6 px-4'>
           <p className='text-[#475569] text-sm'>
             Showing {data?.page === 1 ? 1 : ((data?.page - 1) * 10)} - {data?.page === 1 ? data?.docs?.length : ((data?.page - 1) * 10) + data?.docs?.length} of {data?.totalDocs} results
