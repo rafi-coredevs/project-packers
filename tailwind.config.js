9c4d36d23d9daab18ff9ac6bbc0414bc52a0770e
yeasir-coredevs <yeasir_hossain@coredevs.ltd>
2023-09-04 12:10:21 +0600

    order loading fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {
-//       colors:{
-//         primary : '#F2C852',
-//         secondary: '#0D3D4B'
-
-//       },
-//       content:{
-//         'arrowLeft': 'url(./src/assets/icons/cd-arrow-right-2.svg)'
-//       }
-//     },
-//     fontFamily: {
-//       'sans': ['Plus Jakarta Sans', "sans-serif"],
-//       'sora': ['Sora', "sans-serif"],
-//       'inter': ['Inter', 'sans-serif']
-//     }
-//   },
-//   plugins: [],
-// }
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
