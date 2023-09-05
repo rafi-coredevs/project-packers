cce782db5fef8bd1eb81274fdf13d908d2798d54
yeasir-coredevs <yeasir_hossain@coredevs.ltd>
2023-09-05 12:23:20 +0600

    user ctx updated

--- src/contexts/user/UserContext.jsx
+++ src/contexts/user/UserContext.jsx
@@ -7,5 +7,4 @@ export const UserProvider = ({ children }) => {
 	return <UserContext.Provider value={useUser()} > {children}</UserContext.Provider>;
 };
 
-// eslint-disable-next-line react-refresh/only-export-components
 export const useUserCtx = () => useContext(UserContext);
